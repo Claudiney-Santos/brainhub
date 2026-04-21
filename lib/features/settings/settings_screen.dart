d776914df839972ad8ea05b9157dd0ed0113676a
Thales Janisch Santos <thales3006@gmail.com>
2026-04-21 11:44:38 -0300

    feat: added basic settings template screen

---
+++ lib/features/settings/settings_screen.dart
@@ -0,0 +1,31 @@
+import 'package:brainhub/router/app_router.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => context.go(AppRouter.menu),
+        ),
+        title: const Text('Settings'),
+      ),
+      body: const Center(child: Text('Settings Screen')),
+    );
+  }
+}
