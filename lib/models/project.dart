af0b618e10c730de3ae4f18a2696937c38101308
Thales Janisch Santos <thales3006@gmail.com>
2026-05-18 00:01:29 -0300

    feat: hive support for project class

--- lib/models/project.dart
+++ lib/models/project.dart
@@ -1,5 +1,13 @@
+import 'package:hive_ce_flutter/hive_flutter.dart';
+
+part 'project.g.dart';
+
+@HiveType(typeId: 0)
 class Project {
+  @HiveField(0)
   final String name;
+
+  @HiveField(1)
   final String code;
 
   Project({required this.name, required this.code});
