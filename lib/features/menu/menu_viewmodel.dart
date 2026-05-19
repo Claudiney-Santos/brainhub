c890d7020d13b98a9c251aa939165ddfed7876f4
Thales Janisch Santos <thales3006@gmail.com>
2026-05-19 02:05:56 -0300

    feat: menu project with supabase

--- lib/features/menu/menu_viewmodel.dart
+++ lib/features/menu/menu_viewmodel.dart
@@ -1,64 +1,45 @@
-import 'dart:collection';
-
 import 'package:brainhub/models/project.dart';
 import 'package:brainhub/repositories/projects_repository.dart';
-import 'package:brainhub/utils/pair.dart';
 import 'package:brainhub/utils/result.dart';
 import 'package:flutter/material.dart';
 
 class MenuViewModel extends ChangeNotifier {
   bool isLoaded = false;
-  HashMap<String, Project> projects = HashMap();
+  List<Project> projects = [];
+
   final ProjectsRepository _projectsRepository;
 
   MenuViewModel({required ProjectsRepository projectsRepository})
     : _projectsRepository = projectsRepository;
 
-  List<Pair<String, Project>> get projectsList =>
-      projects.entries.map((entry) => Pair(entry.key, entry.value)).toList();
-
   void load() async {
-    isLoaded = false;
-    projects = HashMap();
-    final projectsMap = await _projectsRepository.loadProjects();
-    for (final id in projectsMap.keys) {
-      projects[id] = projectsMap[id]!;
-    }
-    isLoaded = true;
-    notifyListeners();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      isLoaded = false;
+      notifyListeners();
+      projects = (await _projectsRepository.loadProjects()).toList();
+      isLoaded = true;
+      notifyListeners();
+    });
   }
 
   Future<Result<(), String>> addProject(String projectName) async {
-    if (_projectsRepository.getProjectByName(projectName) != null) {
-      return Result.err('"$projectName" already exists.');
-    }
-
     isLoaded = false;
     notifyListeners();
-
-    await _projectsRepository.addProject(projectName);
+    final result = await _projectsRepository.addProject(projectName);
     load();
-
-    return Result.ok(());
+    return result;
   }
 
   Future<Result<(), String>> renameProject(String id, String newName) async {
     try {
-      final oldProject = _projectsRepository.getProjectById(id)!;
-      final oldName = oldProject.name;
+      final oldProject = projects.firstWhere((p) => p.id == id);
+      if (oldProject.name == newName) return Result.ok(());
 
-      if (oldName == newName) {
-        return Result.ok(());
-      }
-
-      final newProject = Project(name: newName, code: oldProject.code);
-
+      final newProject = oldProject.copyWith(name: newName);
       isLoaded = false;
       notifyListeners();
-
       final result = await _projectsRepository.updateProject(id, newProject);
       load();
-
       return result;
     } catch (e) {
       return Result.err(e.toString());
@@ -69,12 +50,11 @@ class MenuViewModel extends ChangeNotifier {
     try {
       isLoaded = false;
       notifyListeners();
-
       await _projectsRepository.removeProject(id);
       load();
       return Result.ok(());
-    } catch (error) {
-      return Result.err(error.toString());
+    } catch (e) {
+      return Result.err(e.toString());
     }
   }
 }
